2cd31f9e81538ab8a73fdc0bf7a5046f5a4ef6e1
Katja <Katja@katjas-air.local>
2018-10-08 15:10:43 +0200

    Aufgabe0

---
+++ Aufgabe0.js
@@ -0,0 +1,19 @@
+/**
+Aufgabe 0
+Katja Kristina Ljubas
+Matrikelnummer: 259521
+Datum: 07.10.2018
+Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
+**/
+
+
+function Feldeingabe() {
+    var Name = prompt("Gib deinen Namen hier ein");
+    var node = document.getElementById("innerhtmlcontent");
+    node.innerHTML += "Hey ";
+    node.innerHTML += Name;
+    node.innerHTML += " was geht?";
+    console.log("Hey " + Name + "was geht?");
+}
+document.addEventListener('DOMContentLoaded', Feldeingabe);
+//# sourceMappingURL=Aufgabe0.js.map
