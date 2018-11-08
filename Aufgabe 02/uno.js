c7d25a77ab765ff55cf2d3c1825354e0cac36466
Katja <Katja@192.168.0.34>
2018-11-08 09:12:05 +0100

    Aufgabe 2 neu

--- Aufgabe 02/uno.js
+++ Aufgabe 02/uno.js
@@ -4,28 +4,28 @@ Matrikelnummer: 259521
 Datum: 28.10.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-var Unospiel;
-(function (Unospiel) {
+var uno;
+(function (uno) {
     let colors = ["red", "blue", "green", "yellow"];
-    let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "<=>"];
-    let cards = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }];
+    let values = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "X", "<=>"];
+    let deck = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "wish" }, { color: "black", value: "wish" }, { color: "black", value: "wish" }, { color: "black", value: "wish" }];
     for (let i = 0; i < colors.length; i++) {
         for (let j = 0; j < values.length; j++) {
-            for (let k = 0; k < 2; k++) {
+            for (let z = 0; z < 2; z++) {
                 let card = { color: colors[i], value: values[j] };
-                cards.push(card);
+                deck.push(card);
             }
         }
     }
-    console.log(cards);
+    console.log(deck);
     let handcards = [];
-    function Eingabe() {
-        var kartenanzahl = prompt("Kartenanzahl");
+    function select() {
+        var kartenanzahl = prompt("Gib die Anzahl deiner Handkarten an");
         let n = parseInt(kartenanzahl);
         for (let anz = n; anz > 0; anz--) {
-            let r = Math.floor(Math.random() * (cards.length - 1));
-            handcards.push(cards[r]);
-            cards.splice(r, 1);
+            let r = Math.floor(Math.random() * (deck.length - 1));
+            handcards.push(deck[r]);
+            deck.splice(r, 1);
         }
         for (let b = 0; b < handcards.length; b++) {
             let div = document.createElement("div");
@@ -35,6 +35,6 @@ var Unospiel;
             div.classList.add(handcards[b].color);
         }
     }
-    document.addEventListener("DOMContentLoaded", Eingabe);
-})(Unospiel || (Unospiel = {}));
+    document.addEventListener("DOMContentLoaded", select);
+})(uno || (uno = {}));
 //# sourceMappingURL=uno.js.map
--- Aufgabe 02/uno.ts
+++ Aufgabe 02/uno.ts
@@ -4,47 +4,53 @@ Matrikelnummer: 259521
 Datum: 28.10.2018
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-namespace Unospiel {
-    interface Unocard {
+namespace uno{
+    
+    interface Card {
         color: string;
         value: string;
-    }
-
-    let colors: string[] = ["red", "blue", "green", "yellow"];
-    let values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "X", "<=>"];
-    let cards: Unocard[] = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }, { color: "black", value: "FW" }];
-
-    for (let i: number = 0; i < colors.length; i++) {
-        for (let j: number = 0; j < values.length; j++) {
-            for (let k: number = 0; k < 2; k++) {
-                let card: Unocard = { color: colors[i], value: values[j] };
-                cards.push(card);
+        }
+    
+    let colors: string[] =["red", "blue","green","yellow"]
+    let values : string[] = ["1","2","3","4","5","6","7","8","9","X","<=>"]
+    let deck: Card[] =[{color:"red", value:"0"},{color:"blue", value:"0"} , {color:"green", value:"0"} , {color:"yellow", value:"0"} , {color:"black", value:"+4"} , {color:"black", value:"+4"} , {color:"black", value:"+4"} , {color:"black", value:"+4"}, {color:"black", value:"wish"} , {color:"black", value:"wish"} , {color:"black", value:"wish"} , {color:"black", value:"wish"}];
+    
+        
+        
+    for(let i: number = 0; i<colors.length;i++){
+        for(let j : number = 0; j < values.length; j++){
+            for(let z: number = 0; z < 2 ; z++){
+                let card: Card ={color: colors[i], value: values[j] };
+                deck.push(card);
+                }
             }
         }
-    }
-    console.log(cards);
-
-    let handcards: Unocard[] = [];
-
-    function Eingabe(): void {
-        var kartenanzahl: string = prompt("Kartenanzahl");
+    console.log(deck);
+    
+    let handcards:Card[] = [];
+    
+     
+     
+    
+    function select(): void {
+        var kartenanzahl: string = prompt("Gib die Anzahl deiner Handkarten an");
         let n: number = parseInt(kartenanzahl);
-
+     
+  
         for (let anz: number = n; anz > 0; anz--) {
-            let r: number = Math.floor(Math.random() * (cards.length - 1));
-            handcards.push(cards[r]);
-            cards.splice(r, 1);
+            let r: number = Math.floor(Math.random() * (deck.length - 1));
+            handcards.push(deck[r]);
+            deck.splice(r, 1);
         }
-
-        for (let b: number = 0; b < handcards.length; b++) {
+          for (let b: number = 0; b < handcards.length; b++) {
             let div: HTMLElement = document.createElement("div");
             document.getElementById("Handkarten").appendChild(div);
             div.innerHTML = handcards[b].value;
             div.classList.add("Handkarten");
             div.classList.add(handcards[b].color);
+            }
+    
+    
         }
-        
-    }
-    document.addEventListener("DOMContentLoaded", Eingabe);
- 
-}
+     document.addEventListener("DOMContentLoaded", select);
+    }   
